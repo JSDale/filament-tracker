1eaaa08e4fc5e35fafffbecf3f52c7f6d4896f33
JSDALE <jdale994@googlemail.com>
2024-11-06 20:41:32 +0000

    added a wireframe

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` || '.env.pub'});
 const Express = require('express');
 const app = Express();
 const Path = require('path');
+const
+{
+    PORT
+} = process.env;
 
-const Port = 3001;
+var port = PORT;
+if (typeof port == 'undefined')
+{
+    port = 3001;
+}
 
 app.use(express.static(Path.join(__dirname, 'views')));
 app.use(express.static(Path.join(__dirname, 'public')));
@@ -13,5 +22,5 @@ app.get('/', MainController.main);
 
 app.listen(port, () =>
 {
-    console.debug("starting on: " + Port);
+    console.debug("starting on: " + port);
 });
